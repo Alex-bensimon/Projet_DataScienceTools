ecabeb7219d094cdfe48c12db0c2802644f4299f
Victor Henrio <pro.henrio@gmail.com>
2020-06-16 18:07:43 +0200

    maj

--- script_imdb(tuto).py
+++ script_imdb(tuto).py
@@ -80,22 +80,13 @@ for _ in range(5):
 movie_ratings = analy.data_frame_netoyage(names,years,imdb_ratings,metascores,votes,categories)
 
   
-        
-        # fig, axes = plt.subplots(nrows = 1, ncols = 3, figsize = (16,4))
-        # ax1, ax2, ax3 = fig.axes
-        # ax1.hist(movie_ratings['imdb'], bins = 10, range = (0,10)) # bin range = 1
-        # ax1.set_title('IMDB rating')
-        # ax2.hist(movie_ratings['metascore'], bins = 10, range = (0,100)) # bin range = 10
-        # ax2.set_title('Metascore')
-        # ax3.hist(movie_ratings['n_imdb'], bins = 10, range = (0,100), histtype = 'step')
-        # ax3.hist(movie_ratings['metascore'], bins = 10, range = (0,100), histtype = 'step')
-        # ax3.legend(loc = 'upper left')
-        # ax3.set_title('The Two Normalized Distributions')
-        # for ax in fig.axes:
-        #     ax.spines['top'].set_visible(False)
-        #     ax.spines['right'].set_visible(False)   
-        # plt.show()
 
+#%%
+
+#Partie Analyse 
+
+import numpy as np 
+import pandas as pd
 
 
 
