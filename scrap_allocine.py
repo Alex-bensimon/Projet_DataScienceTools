86cd650f220a68e79d4c501b6fa7f079b5d616ca
Victor Henrio <pro.henrio@gmail.com>
2020-06-16 19:05:24 +0200

    maj_sur _scraping_allociné

--- scrap_allocine.py
+++ scrap_allocine.py
@@ -10,65 +10,85 @@ import matplotlib.pyplot as plt
 import seaborn as sns; sns.set(style="ticks", color_codes=True)
    
 titre = []
-nb_e = []
-date = []
+nb_entrees = []
+date_sortie = []
 genre = []
 duree = []
-np = []
-ns = []
+note_spect = []
+note_presse = []
 realisateur = []
 acteurs_principaux = []
 classement = []
 n = []
 
-i=1
-for i in range(10):
-    page_number = i
-    page_link = f'http://www.allocine.fr/film/meilleurs/boxoffice/?page={page_number}'
+pages = [1,2,3,4,5]
+page = 1
+
+for page in pages:
+   
+    page_link = f'http://www.allocine.fr/film/meilleurs/boxoffice/?page={page}'
+    
     response = requests.get(page_link)
+    
     html = bs4.BeautifulSoup(response.text, 'html.parser')
     
-    for div in html.find_all('div', attrs={'class':'card entity-card entity-card-list cf'}):
-        film_title = div.find('a', {"class": "meta-title-link"})
-        titre.append(film_title.text)
-        date_sortie = div.find('span', {"class": "date"})
-        if date_sortie != None:
-            date.append(date_sortie.text)
-        else:
-            date.append(None) 
-        nb_entrees = div.find('div', {"class": "bo"})
-        nb_e.append(int(nb_entrees.text.replace(' ','').replace('entrées','')))
-
-
-        for cl in div.find_all('div', attrs={'class':"rating-item-content"}):
-            type_note = cl.find('a', {"class": "xXx rating-title"})
-            print(type_note.text)
-            note = cl.find('span', {"class": "stareval-note"})
-            if note != None:    
-                np.append(float(note.text.replace(',','.')))
+    mv_containers = html.find_all('div', attrs={'class':'card entity-card entity-card-list cf'})
+    
+    for container in mv_containers:    
+        
+        if container.find('div', class_ = 'ratings-metascore') is not None:
+              
+            #Scrap the title 
+            film_title = container.find('a', {"class": "meta-title-link"})
+            titre.append(film_title.text)
+            
+            #Scrap the release date
+            date_sortie = container.find('span', {"class": "date"})           
+            if date_sortie != None:
+                date_sortie.append(date_sortie.text)  # peu interressant car date_reprise != date sortie
+            else:
+                date_sortie.append(None) 
+            
+            #Scrap number of admission
+            nb_entrees = container.find('div', {"class": "bo"})
+            nb_entrees.append(int(nb_entrees.text.replace(' ','').replace('entrées','')))
+            
+            #Srap the ratting
+            rating_container = container.find_all('div', attrs={'class':"rating-item-content"})
+    
+            for section in rating_container:
+                
+                note = section.find('span', {"class": "stareval-note"}).text
+                
+                
+                if note != None:    
+                    note.append((note.text.replace(',','.')))
+                else:
+                    note_presse.append(None)
+    
+                if note != None:        
+                    note_spect.append((note.text.replace(',','.')))
+                else:
+                    note_spect.append(None)
+                    
+                    
+                    
+            """     
+            note_presse = div.find('span', {"class": "stareval-note"})
+            if note_presse != None:    
+                np.append(float(note_presse.text.replace(',','.')))
             else:
                 np.append(None)
-            if note != None:        
-                ns.append(float(note.text.replace(',','.')))
+            note_spect = div.find('span', {"class": "stareval-note"})
+            if note_spect != None:        
+                ns.append(float(note_spect.text.replace(',','.')))
             else:
                 ns.append(None)
-        """     
-        note_presse = div.find('span', {"class": "stareval-note"})
-        if note_presse != None:    
-            np.append(float(note_presse.text.replace(',','.')))
-        else:
-            np.append(None)
-        note_spect = div.find('span', {"class": "stareval-note"})
-        if note_spect != None:        
-            ns.append(float(note_spect.text.replace(',','.')))
-        else:
-            ns.append(None)
-        """
+            """
     
     
-df = pd.DataFrame({'Titre ':titre,'Date de sortie':date,
-                        'Nombre entrées':nb_e,'Note presse':np,
-                        'Note spect':ns})
+df = pd.DataFrame({'Titre ':titre,'Date de sortie':date_sortie,
+                        'Nombre entrées':nb_entrees,'Note presse':note_presse })
 print(note)
 #print(df)
 print(df.describe())
