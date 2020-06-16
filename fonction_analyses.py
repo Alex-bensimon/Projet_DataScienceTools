699245b02f31871085eab92fbbbe8884bd92f28d
Victor Henrio <pro.henrio@gmail.com>
2020-06-16 17:42:00 +0200

    filtrage_film

--- fonction_analyses.py
+++ fonction_analyses.py
@@ -6,7 +6,7 @@ Created on Tue Jun 16 11:45:39 2020
 """
 import pandas as pd
 
-def data_frame_netoyage(names,years,imdb_ratings,metascores,votes):
+def data_frame_netoyage(names,years,imdb_ratings,metascores,votes,categories):
     '''
     Create a new dataFrame with all the inforamtion about the movie from the scraping
     
@@ -26,7 +26,8 @@ def data_frame_netoyage(names,years,imdb_ratings,metascores,votes):
         'year': years,
         'imdb': imdb_ratings,
         'metascore': metascores,
-        'votes': votes  
+        'votes': votes,
+        'categories':categories
     })
     
     print(movie_ratings.info())
@@ -37,6 +38,26 @@ def data_frame_netoyage(names,years,imdb_ratings,metascores,votes):
     
     movie_ratings['n_imdb'] = movie_ratings['imdb'] * 10
     
-    movie_ratings.to_csv('movie_ratings.csv')
+    movie_ratings.to_csv('movie_ratings2.csv')
+    
+    print(movie_ratings.head(10))
     
     return movie_ratings
+
+
+#def plot_graph():
+
+        # fig, axes = plt.subplots(nrows = 1, ncols = 3, figsize = (16,4))
+        # ax1, ax2, ax3 = fig.axes
+        # ax1.hist(movie_ratings['imdb'], bins = 10, range = (0,10)) # bin range = 1
+        # ax1.set_title('IMDB rating')
+        # ax2.hist(movie_ratings['metascore'], bins = 10, range = (0,100)) # bin range = 10
+        # ax2.set_title('Metascore')
+        # ax3.hist(movie_ratings['n_imdb'], bins = 10, range = (0,100), histtype = 'step')
+        # ax3.hist(movie_ratings['metascore'], bins = 10, range = (0,100), histtype = 'step')
+        # ax3.legend(loc = 'upper left')
+        # ax3.set_title('The Two Normalized Distributions')
+        # for ax in fig.axes:
+        #     ax.spines['top'].set_visible(False)
+        #     ax.spines['right'].set_visible(False)   
+        # plt.show()
