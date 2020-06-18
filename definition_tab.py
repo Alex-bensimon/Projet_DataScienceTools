274311bf36d814e9832a8fcc93585a6d264754d3
Victor Henrio <pro.henrio@gmail.com>
2020-06-18 18:52:00 +0200

    fct index acteurs beug

--- definition_tab.py
+++ definition_tab.py
@@ -5,6 +5,8 @@ Created on Thu Jun 18 14:24:27 2020
 @author: Victor HENRIO
 """
 
+import pandas as pd
+
 def instanciation_tablist():
     
         # Tabs 
@@ -28,3 +30,39 @@ def instanciation_tablist():
     mv_attributs =  names,years,imdb_ratings,metascores,votes,categories,mv_pages,genres,stars,rank,nb_oscar,win,nom,runtime,budget,gross
     
     return mv_attributs
+
+
+
+def creation_dataframe(mv_attributs):
+    '''
+    Create a new dataFrame with all the inforamtion about the movie from the scraping
+
+    :param tupe mv_attributs: tupe of all the data frome movies
+    :return: A pandas Dataframe with all the informations about the movies
+    :rtype: pandas.dataframe
+
+    '''
+
+    movie_ratings = pd.DataFrame({
+        'movie': mv_attributs[0],
+        'year': mv_attributs[1],
+        'imdb_ratings': mv_attributs[2],
+        'metascore': mv_attributs[3],
+        'votes': mv_attributs[4],
+        'category': mv_attributs[5],
+        'mv_page': mv_attributs[6],
+        'genres' : mv_attributs[7],
+        'stars' : mv_attributs[8],
+        'rank' : mv_attributs[9],
+        'nb_oscar' : mv_attributs[10],
+        'win' : mv_attributs[11],
+        'nom' : mv_attributs[12],
+        'runtime' : mv_attributs[13],
+        'budget' : mv_attributs[14],
+        'gross' : mv_attributs[15]
+    })
+    
+    movie_ratings.loc[:, 'year'] = movie_ratings['year'].astype(int)
+    movie_ratings['n_ratings'] = movie_ratings['imdb_ratings'] * 10   
+    
+    return movie_ratings
--- fonction_analyses.py
+++ fonction_analyses.py
@@ -3,53 +3,23 @@
 Created on Tue Jun 16 11:45:39 2020
 @author: Victor HENRIO
 """
-import pandas as pd
+
 import math
+import pandas as pd
 
-
-#def data_frame_netoyage(names,years,imdb_ratings,metascores,votes,categories,mv_pages):
-def data_frame_netoyage(mv_attributs):
+def delete_raws_nan(movie_ratings):
     '''
-    Create a new dataFrame with all the inforamtion about the movie from the scraping
-
-    :param tupe mv_attributs: tupe of all the data frome movies
-    :return: A pandas Dataframe with all the informations about the movies
-    :rtype: pandas.dataframe
-
-    '''
-
-    movie_ratings = pd.DataFrame({
-        'movie': mv_attributs[0],
-        'year': mv_attributs[1],
-        'imdb_ratings': mv_attributs[2],
-        'metascore': mv_attributs[3],
-        'votes': mv_attributs[4],
-        'category': mv_attributs[5],
-        'mv_page': mv_attributs[6],
-        'genres' : mv_attributs[7],
-        'stars' : mv_attributs[8],
-        'rank' : mv_attributs[9],
-        'nb_oscar' : mv_attributs[10],
-        'win' : mv_attributs[11],
-        'nom' : mv_attributs[12],
-        'runtime' : mv_attributs[13],
-        'budget' : mv_attributs[14],
-        'gross' : mv_attributs[15]
-    })
+    If there is not a specific attribut we delete the ligne cause
     
-    movie_ratings.loc[:, 'year'] = movie_ratings['year'].astype(int)
-    movie_ratings['n_imdb'] = movie_ratings['imdb_ratings'] * 10   
+    :param1 dataframe movie_ratings: with all the dataframe from movies,
+    :return dataframe movie_ratings: with all the dataframe from movies
+    :rtype: dataframe
     
-    return movie_ratings
-
-
-def delete_nan(movie_ratings):
-    '''
     '''
 
     i = 0
     for note in movie_ratings.itertuples():
-        test = math.isnan(float(note.n_imdb))
+        test = math.isnan(float(note.n_ratings))
         if test is True:
             movie_ratings = movie_ratings.drop(movie_ratings.index[i])
             i -= 1
@@ -57,8 +27,17 @@ def delete_nan(movie_ratings):
     return movie_ratings
 
 
+
+
+
 def replace_metascore(movie_ratings):
     '''
+    We replace the metascore by imdb score if the metascore does not exist
+    
+    :param dataframe movie_ratings: dataframe with all the dataframe from movies
+    :return dataframe movie_ratings:dataframe with all the dataframe from movies
+    :rtype: dataframe
+    
     '''
     i = 0
     for note in movie_ratings.itertuples():
@@ -71,15 +50,72 @@ def replace_metascore(movie_ratings):
     return movie_ratings
 
 
+def replace_nan(movie_ratings):
+    '''
+    
+    '''
+
+
+
+def actors_to_num(movie_ratings):
+    '''
+    '''
+
+    #df = movie_ratings
+    #movie_ratings["stars"]
+    
+    
+    acteurs = []
+    acteurs = movie_ratings["stars"]
+    list_acteur = []
+    index_acteurs = []
+
+    index = 0
+    for act in acteurs:
+        if act in list_acteur:
+            index_acteurs.append(list_acteur.index(act))
+        else: 
+            list_acteur.append(act)
+            index_acteurs.append(index)
+            index += 1
+
+    #table_corres_df = np.array([index_acteurs, temp])
+    len_list_acteur = []
+    i = 0
+    for i in range(len(list_acteur)):
+        len_list_acteur.append(i)
+    print(len_list_acteur)
+    df2 = pd.DataFrame({'Index': len_list_acteur,'Acteur': list_acteur})
+    df2 = df2.set_index('Index')
+    print(df2)
+    df2.to_csv('table_correspondance_acteurs.csv')
+
+
+    actors_to_num('movie_ratings.csv')
+    
+    return index_acteurs
+
+
+
 def clean_dataframe(movie_ratings):
     '''
+    All the procedures that we need to clean the data frame: 
+        drop mv_page, imdb_ratings 
+        set on index movie
+        labelize the category
+        delete line without imdb_ratings
+        replace metascore
+    
+    :param1 movie_ratings: dataframe with all the dataframe from movies
+    :return dataframe movie_ratings: clean dataframe with all the dataframe from movies 
+    :rtype: dataframe
     '''
     
     movie_ratings = movie_ratings.drop(["mv_page"],axis=1)
     movie_ratings = movie_ratings.drop(["imdb_ratings"],axis=1)
     movie_ratings = movie_ratings.set_index('movie')
     movie_ratings['category'] = movie_ratings['category'].replace(regex={'R': '1','PG-13': '3', 'PG': '2'})
-    movie_ratings = delete_nan(movie_ratings)
+    movie_ratings = delete_raws_nan(movie_ratings)
     movie_ratings = replace_metascore(movie_ratings)
 
     return movie_ratings
